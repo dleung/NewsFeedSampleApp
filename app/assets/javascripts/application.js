05b6668a51068ed027381593fa09f7f12b3fe793
David Leung <dleung@quosap.com>
2012-08-19 17:50:29 -0700

    removed more files

--- app/assets/javascripts/application.js
+++
@@ -1,7 +0,0 @@
-//= require jquery
-//= require jquery_ujs
-//= require jquery-ui
-//= require gritter
-//= require chosen.jquery
-//= require jquery.dataTables.min
-//= require jquery.cookie.js
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-NewsFeedSampleApp::Application.config.secret_token = 'fbc13e35d051fa2ed15d957d7c0d0d3c5d755885ec1ceb3c4c654c8a643f8ba2b6d2452f18dd835e4f7c8a5f88c968303a4fd71a11a6752ae1d2f628c9670cd9'
